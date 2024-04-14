be639d9db6436789c46714dd252e90663a29dc76
Maria2009gift <kovalenkom2009@gmail.com>
2024-04-14 22:53:25 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+
+// =====1=====
+
+const delay = ms => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(`Resolved after ${ms}`)
+    }, ms)
+    })
+  }
+  
+  const l = time => {console.log(`Resolved after ${time}ms`)}
+  
+  // Виклич функції для перевірки
+  delay(2000).then(l) // Resolved after 2000ms
+  delay(1000).then(l) // Resolved after 1000ms
+  delay(1500).then(l) // Resolved after 1500ms
+
+// =====2=====
+
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+]
+  
+const toggleUserState = (allUsers, userName) => {
+    return new Promise ((resolve, reject) => {
+        
+        resolve(allUsers.map(user =>
+            user.name === userName ? { ...user, active: !user.active } : user,
+        ))
+    })
+  }
+  
+  const log = updatedUsers => console.table(updatedUsers)
+
+  toggleUserState(users, 'Mango').then(log)
+  toggleUserState(users, 'Lux').then(log)
+
+// =====3=====
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  };
+  
+  const makeTransaction = (transaction) => {
+    const delay = randomIntegerFromInterval(200, 500)
+    
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const canProcess = Math.random() > 0.3
+      
+          if (canProcess) {
+            resolve(transaction.id, delay)
+          } else {
+            reject(transaction.id)
+          }
+        }, delay)
+    })
+  };
+  
+  const logSuccess = (id, time) => {
+    console.log(`Transaction ${id} processed in ${time}ms`)
+  }
+  
+  const logError = id => {
+    console.warn(`Error processing transaction ${id}. Please try again later.`)
+  }
+  
+
+
+  makeTransaction({ id: 70, amount: 150 })
+    .then(logSuccess)
+    .catch(logError)
+  
+  makeTransaction({ id: 71, amount: 230 })
+    .then(logSuccess)
+    .catch(logError)
+  
+  makeTransaction({ id: 72, amount: 75 })
+    .then(logSuccess)
+    .catch(logError)
+  
+  makeTransaction({ id: 73, amount: 100 })
+    .then(logSuccess)
+    .catch(logError)
+
+
+
+
+
+
+
